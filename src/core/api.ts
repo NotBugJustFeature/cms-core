ee7cc787dcd6846ce992db031d8bfdb11e73af72
Simon Adam <adam.simon@smartbits.hu>
2024-10-15 20:10:39 +0200

    api progress 2

--- src/core/api.ts
+++ src/core/api.ts
@@ -1,5 +1,28 @@
 import { Hono } from 'hono'
 import { prisma } from './prisma'
+import { Prisma } from '@prisma/client'
+import { loadSchema } from '../utils/schemaHandler'
+import { SchemaJson } from '../types/zod'
+
+let schema: SchemaJson = loadSchema('schema_res.json')
+let relations: Record<string, string[]> = {}
+schema.relations.forEach((field) => {
+    if (!Object.keys(relations).includes(field.source_entity)) {
+        relations[field.source_entity] = []
+    }
+    if (!Object.keys(relations).includes(field.target_entity)) {
+        relations[field.target_entity] = []
+    }
+    relations[field.source_entity].push(field.target_entity)
+    relations[field.target_entity].push(field.source_entity)
+})
+// let relations2: Record<string, Record<string, boolean>> = {}
+// Object.keys(relations).forEach((key) => {
+
+//     relations[key].forEach((item) => relations[key][key2] = true)
+
+// console.log('relations', relations)
+
 export const apiApp = new Hono()
 
 apiApp.get('/:collection', async (context) => {
