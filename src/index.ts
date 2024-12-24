f3f6cde680954b589061b4d7930361dae8ccdb66
Simon Adam <adam.simon@smartbits.hu>
2024-12-24 15:55:30 +0100

    update api init

--- src/index.ts
+++ src/index.ts
@@ -4,10 +4,12 @@ import { apiApp } from './api'
 
 const app = new Hono()
 
-app.route('/api', apiApp)
-
-app.get('/', (context) => context.json({ hello: 'world' }))
-app.get('/api/test', (context) => context.json({ hello: 'test' }))
-serve({ port: 3000, fetch: app.fetch }, (i) => console.log(`listening on port ${i.port}...`))
-
-export default app
+export default {
+    app,
+    setup: (port = 3000) => {
+        app.get('/', (context) => context.json({ hello: 'world' }))
+        app.route('/api', apiApp)
+        app.get('/api/test', (context) => context.json({ hello: 'test' }))
+        serve({ port, fetch: app.fetch }, (i) => console.log(`listening on port ${i.port}...`))
+    }
+}
