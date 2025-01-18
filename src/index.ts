989ddb5a734149090ab8a0794a457161688e8c0d
Simon Adam <adam.simon@smartbits.hu>
2025-01-18 17:06:59 +0100

    feat: enhance server setup with CORS and static file serving
    - Added CORS support for API requests from localhost.
    - Implemented static file serving for the admin panel from the './dist/admin' directory.
    - Updated the server setup to include new middleware for improved functionality.

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import { Hono } from 'hono'
 import { serve } from '@hono/node-server'
+import { serveStatic } from '@hono/node-server/serve-static'
+import { cors } from 'hono/cors'
 import { apiApp } from './api'
 
 const app = new Hono()
@@ -7,9 +9,20 @@ const app = new Hono()
 export default {
     app,
     setup: (port = 3000) => {
+        // Enable CORS
+        app.use('/*', cors({
+            origin: ['http://localhost:5173'],
+            credentials: true
+        }))
+
+        // API routes
         app.get('/', (context) => context.json({ hello: 'world' }))
         app.route('/api', apiApp)
         app.get('/api/test', (context) => context.json({ hello: 'test' }))
+
+        // Serve admin panel
+        app.use('/admin/*', serveStatic({ root: './dist/admin' }))
+
         serve({ port, fetch: app.fetch }, (i) => console.log(`listening on port ${i.port}...`))
     }
 }
