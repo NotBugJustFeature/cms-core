15477343056a58b5bb5e262a9ec65db269975119
Simon Adam <adam.simon@smartbits.hu>
2025-03-15 15:34:01 +0100

    add generic permission handler

--- src/utils/permission.ts
+++ src/utils/permission.ts
@@ -2,58 +2,63 @@ type TRoles = 'user' | 'admin' | '*'
 type TCollections = 'user' | 'post' | 'comment' | '*'
 type TScope = 'all' | 'own'
 
-type ValueOrArray<T> = T | T[]
-
 type Combination<T extends string, U extends string = ''> = T extends `${infer F}${infer R}`
     ? Combination<R, `${U}`> | Combination<R, `${U}${F}`>
     : U
 type TCrud = 'c' | 'r' | 'u' | 'd'
 export type TCrudCombination = Exclude<Combination<'crud'>, ''> | '*'
 
-type Permission = `${TRoles}:${TCollections}:${TCrudCombination}:${TScope}`
+type Permission<
+    R extends string = TRoles,
+    C extends string = TCollections
+> = `${R}:${C}:${TCrudCombination}:${TScope}`
 
-type TPermissionStore = {
-    roles: TRoles
-    collection: TCollections
+type TPermissionStore<R extends string = TRoles, C extends string = TCollections> = {
+    roles: R | '*'
+    collection: C | '*'
     crud: Record<TCrud, boolean>
     scope: TScope
     validator?: undefined | (() => boolean)
 }
 
-class PermissionHandler {
-    private static instance: PermissionHandler
-    private permissions: Array<TPermissionStore>
+class PermissionHandler<R extends string = TRoles, C extends string = TCollections> {
+    private static instance: typeof PermissionHandler
+    private permissions: Array<TPermissionStore<R, C>>
 
     private constructor() {
-        this.permissions = new Array<TPermissionStore>()
+        this.permissions = new Array<TPermissionStore<R, C>>()
     }
 
-    public static getInstance(): PermissionHandler {
+    public static getInstance<
+        R extends string = TRoles,
+        C extends string = TCollections
+    >(): PermissionHandler<R, C> {
         if (!PermissionHandler.instance) {
-            PermissionHandler.instance = new PermissionHandler()
+            PermissionHandler.instance = new PermissionHandler<
+                R,
+                C
+            >() as unknown as typeof PermissionHandler
         }
-        return PermissionHandler.instance
+        return PermissionHandler.instance as unknown as PermissionHandler<R, C>
     }
 
     private permissionSplitter(
-        permission: Permission
-    ): [TRoles, TCollections, TCrudCombination, TScope] {
+        permission: Permission<R, C>
+    ): [R | '*', C | '*', TCrudCombination, TScope] {
         const [roles, collection, crud, scope] = permission.split(':')
         return [
-            roles as TRoles,
-            collection as TCollections,
+            roles as R | '*',
+            collection as C | '*',
             crud == '*' ? 'crud' : (crud as TCrudCombination),
             scope as TScope
         ]
     }
 
-    public addPermission(permission: Permission, validator?: undefined | (() => boolean)) {
+    public addPermission(permission: Permission<R, C>, validator?: undefined | (() => boolean)) {
         const [roles, collection, crud, scope] = this.permissionSplitter(permission)
-        // console.log(collection, crud.split(''), scope)
         const crudArray = crud.split('')
         const crudRes = ['c', 'r', 'u', 'd'].reduce(
             (acc, curr) => ({
-                // @ts-ignore
                 ...acc,
                 [curr]: crudArray.includes(curr)
             }),
@@ -69,17 +74,19 @@ class PermissionHandler {
             validator
         })
     }
+
     private equalsOrProvided(a: any, b: any): boolean {
         if (a == b || a == '*' || b == '*') return true
-
         return false
     }
+
     private crudSatisfies(a: Record<TCrud, boolean>, b: TCrudCombination): boolean {
         const bArray = b.split('')
         //@ts-ignore
         return bArray.every((b) => Boolean(a[b]))
     }
-    public hasPermission(permission: Permission): boolean {
+
+    public hasPermission(permission: Permission<R, C>): boolean {
         const [roles, collection, crud, scope] = this.permissionSplitter(permission)
         for (const perm of this.permissions) {
             if (
@@ -95,4 +102,9 @@ class PermissionHandler {
     }
 }
 
-export const permissionHandler = PermissionHandler.getInstance()
+export const permissionHandler = PermissionHandler.getInstance<
+    'user' | 'admin',
+    'post' | 'comment' | 'user'
+>()
+permissionHandler.addPermission('admin:post:c:all', () => true)
+console.log(permissionHandler.hasPermission('user:*:*:all'))
